1cb8746c455af7342f63469411c1c8a017490176
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-01 18:48:45 -0300

    refactor(eslint): remove eslint conflicts and add recomended rules for React Testing Library

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import testingLibrary from 'eslint-plugin-testing-library';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,7 +14,30 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     files: ['**/tests/**/*', '**/*.{test,spec}.*'],
-    ...compat.extends('plugin:testing-library/react')[0],
+    plugins: {
+      'testing-library': testingLibrary,
+    },
+    rules: {
+      // Regras recomendadas do testing-library
+      'testing-library/await-async-queries': 'error',
+      'testing-library/await-async-utils': 'error',
+      'testing-library/no-await-sync-queries': 'error',
+      'testing-library/no-container': 'error',
+      'testing-library/no-debugging-utils': 'warn',
+      'testing-library/no-dom-import': ['error', 'react'],
+      'testing-library/no-node-access': 'error',
+      'testing-library/no-promise-in-fire-event': 'error',
+      'testing-library/no-render-in-lifecycle': 'error',
+      'testing-library/no-unnecessary-act': 'error',
+      'testing-library/no-wait-for-multiple-assertions': 'error',
+      'testing-library/no-wait-for-side-effects': 'error',
+      'testing-library/no-wait-for-snapshot': 'error',
+      'testing-library/prefer-find-by': 'error',
+      'testing-library/prefer-presence-queries': 'error',
+      'testing-library/prefer-query-by-disappearance': 'error',
+      'testing-library/prefer-screen-queries': 'error',
+      'testing-library/render-result-naming-convention': 'error',
+    },
   },
 ];
 
