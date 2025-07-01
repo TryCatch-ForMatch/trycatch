0e309afa90c9a4e7f7315e4a661a8ede0aa8bb3d
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-01 18:07:51 -0300

    fix(jest): revert test environment to jsdom

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 
 const config: Config = {
   coverageProvider: 'v8',
-  testEnvironment: 'jest-fixed-jsdom',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
