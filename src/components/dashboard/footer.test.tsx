ca262a6523c1bca50fdd82570c916fe3ce41e2a1
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-01 18:47:32 -0300

    test(dashboard footer): add unit test for DashboardFooter component

---
+++ src/components/dashboard/footer.test.tsx
@@ -0,0 +1,12 @@
+import { DashboardFooter } from '@/components/dashboard/footer';
+import { render, screen } from '@testing-library/react'; // TODO quando o arquivo test-utils.tsx estiver pronto, importar isso de lá
+
+describe('<DashboardFooter/>', () => {
+  it('should render dashboardFooter component', () => {
+    render(<DashboardFooter />);
+
+    const dashboardFooterText = screen.getByRole('contentinfo');
+
+    expect(dashboardFooterText).toBeInTheDocument();
+  });
+});
