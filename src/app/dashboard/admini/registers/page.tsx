c31ec57a6120f020b8bb7b01eb9bc1e78c54b508
Fernanda Barros Linhares <devfernandalinhares@gmail.com>
2025-07-07 21:28:39 -0300

    feat: criação lista skill

--- src/app/dashboard/admini/registers/page.tsx
+++ src/app/dashboard/admini/registers/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+
+import InviteForm from '@/components/dashboard/invite/inviteForm';
+import InviteList from '@/components/dashboard/invite/inviteList';
+import SkillList from '@/components/dashboard/skill/skillList';
+// import SkillForm from "@/components/dashboard/skill/skillForm";
+
 export default function AdminPage() {
   return (
     <main>
       <h1>Admin Page</h1>
+      <InviteForm></InviteForm>
+      <InviteList></InviteList>
+      <SkillList></SkillList>
+      {/* <SkillForm></SkillForm> */}
     </main>
   );
 }
