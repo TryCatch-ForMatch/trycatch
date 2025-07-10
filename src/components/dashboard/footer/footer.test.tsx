7b08e86f259c5b3faf24925630e5721a43b18511
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-10 16:22:58 -0300

    test(dashboard footer): add correct import path for DashboardFooter

--- src/components/dashboard/footer/footer.test.tsx
+++ src/components/dashboard/footer/footer.test.tsx
@@ -1,5 +1,7 @@
-import { DashboardFooter } from '@/components/dashboard/footer/footer';
+import { DashboardFooter } from '@/components/dashboard/footer';
 import { render, screen } from '@testing-library/react'; // TODO quando o arquivo test-utils.tsx estiver pronto, importar isso de lá
+
+// TODO evoluir esse teste conforme o projeto escala
 
 describe('<DashboardFooter/>', () => {
   it('should render dashboardFooter component', () => {
