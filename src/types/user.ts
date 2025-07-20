fcbf9862074179a32a4411ffc4e6315106c8936e
Karina <karinaperesg@gmail.com>
2025-07-20 11:59:37 -0300

    feat(types): inclusão de nova interface User

--- src/types/user.ts
+++ src/types/user.ts
@@ -4,6 +4,15 @@ export interface BasicUser {
   email: string;
 }
 
+export interface User extends BasicUser {
+  password?: string;
+  avatar?: string;
+  linkedin?: string;
+  github?: string;
+  bio?: string;
+  skills?: { skill: { id: string; name: string } }[];
+}
+
 export interface FullUser extends BasicUser {
   avatar?: string;
   linkedin?: string;
