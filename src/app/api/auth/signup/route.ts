2d0e857316a545585e3fadb85ea6613c69747ecf
Karina <karinaperesg@gmail.com>
2025-08-16 13:33:28 -0300

    feat(avatar-signup-user): foi movido o  upload do  avatar do api/auth/signup para api/user/[id]
    feat #247

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -8,7 +8,7 @@ const createUserSchema = z.object({
   name: z.string().min(1, 'O nome é obrigatório.'),
   email: z.string().email('Email inválido.'),
   password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres.'),
-  avatar: z.union([z.string().url(), z.literal('')]).nullable(),
+  avatar: z.string(),
   linkedin: z.union([z.string().url(), z.literal('')]).nullable(),
   github: z.union([z.string().url(), z.literal('')]).nullable(),
   bio: z.string().optional(),
