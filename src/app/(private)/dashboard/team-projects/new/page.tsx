7d7bd74ac76c5b94c25340de821c19bde140e9d5
Karina Peres <134704973+karinaperes@users.noreply.github.com>
2025-08-26 15:30:10 -0300

    Feat/project stack selector (#270)
    * feat(project-stack-selector): instalação de dependencias ui
    
    feat #265
    
    * feat(project-stack-selector): criei a page e o componente ProjectForm para testar o novo componente
    
    feat #265
    
    * feat(project-stack-selector): criação do componente ProjectStackSelector
    
    feat #265

--- src/app/(private)/dashboard/team-projects/new/page.tsx
+++ src/app/(private)/dashboard/team-projects/new/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+import { ProjectForm } from '@/components/Dashboard/TeamProject';
+
 export default function TeamProjectNewPage() {
   return (
     <main>
       <h1>New Team Project</h1>
+      <ProjectForm></ProjectForm>
     </main>
   );
 }
