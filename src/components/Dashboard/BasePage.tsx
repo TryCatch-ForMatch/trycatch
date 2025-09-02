4be54bdf84852432529596bae35a752962a71728
Dennis Camela <123247070+dennisDev77@users.noreply.github.com>
2025-09-02 13:52:51 +0100

    fix: 🐛 Regras de redirecionamento de rotas (#286)

--- src/components/Dashboard/BasePage.tsx
+++ src/components/Dashboard/BasePage.tsx
@@ -1,12 +1,8 @@
 import { ReactNode } from 'react';
 import { DashboardHeader } from './Header';
-// import { getServerSession } from 'next-auth';
-// import { authOptions } from '@/lib/auth';
-// import { User } from '@/types/user';
+
 import { Navbar } from '@/components/layout/Navbar';
 export default async function BasePage({ children }: { children: ReactNode }) {
-  //   const session = await getServerSession(authOptions);
-
   return (
     <section className="mn-h-screen flex">
       <Navbar />
