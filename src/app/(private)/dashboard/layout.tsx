7f985a74495b3e20b3903a7dcb46ad4ff29ee7f9
dennisDev77 <diniscamela5@gmail.com>
2025-09-02 12:28:46 +0100

    fix: 🐛 Regras de redirecionamento de rotas
    Este commit Resovole o problemas de recionando de rotas, e ele
    redireciona as rotas de acordo as rotas que foram implementadas  no
    middleware
    
    ✅ Closes: 283

--- src/app/(private)/dashboard/layout.tsx
+++ src/app/(private)/dashboard/layout.tsx
@@ -1,23 +1,9 @@
 import { ReactNode } from 'react';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
-import { redirect } from 'next/navigation';
-
-//import providers
-import { SidebarProvider } from '@/Context/SidebarContextOpenClose';
-
-type DashboardLayoutProps = {
-  children: ReactNode;
-};
 
 export default async function DashboardLayout({
   children,
-}: DashboardLayoutProps) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    redirect('/login');
-  }
-
+}: {
+  children: ReactNode;
+}) {
   return <main>{children}</main>;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,55 @@
+import { NextRequest, NextResponse, MiddlewareConfig } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 export { default } from 'next-auth/middleware';
 
-export const config = {
-  matcher: ['/dashboard/:path*', '/api/:path*'],
+const rotasPublicas = [
+  { path: '/login', autenticado: 'redirect' },
+  { path: '/register', autenticado: 'redirect' },
+  { path: '/signup', autenticado: 'redirect' },
+  { path: '/', autenticado: 'next' },
+];
+
+export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname; //pega o path da requisição
+
+  //pega o token do usuário logado
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  //encontra a rota pública
+  const rotaPublica = rotasPublicas.find((rota) => path == rota.path);
+
+  //se não tiver token e for rota publica, deixa passar
+  if (!token && rotaPublica) return NextResponse.next();
+
+  //se não tiver token e não for rota pblica, redireciona para login
+  if (!token && !rotaPublica) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
+  }
+
+  //se tiver token e for rota pblica login/register, redireciona para dashboard
+  if (token && rotaPublica && rotaPublica.autenticado === 'redirect') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/dashboard';
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    /*
+     * Desta forma iremos executar o middleware em todas as rotas, exceto:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
 };
