09290cfb77387bd3ddda65f90f8a36fd80b85707
Karina <karinaperesg@gmail.com>
2025-11-02 20:00:36 -0300

    feat(model-user-availability): campo isMentor removido do modelo UserAvailability e migration feita
    feat #335

---
+++ prisma/migrations/20251102225813_update_user_availability_ismentor/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isMentor` on the `UserAvailability` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."UserAvailability" DROP COLUMN "isMentor";
