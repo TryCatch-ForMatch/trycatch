1b22c98b4feaa64fa507fc9a382ca5fdaf93c0c9
dennisDev77 <diniscamela5@gmail.com>
2025-11-10 16:25:28 +0100

    feat: mostrar as skills do user logado

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,19 @@
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// cria __dirname no ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const createJestConfig = nextJest({
   dir: './',
 });
 
 const config: Config = {
-  rootDir: './',
   coverageProvider: 'v8',
+  rootDir: __dirname, // caminho absoluto
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
