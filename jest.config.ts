32d56dcae5a25f5e1ce9b8e3bddc55eb8348a09d
dennisDev77 <diniscamela5@gmail.com>
2025-11-10 16:27:01 +0100

    fix: descartando as modificacoes do jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,5 @@
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-// cria __dirname no ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const createJestConfig = nextJest({
   dir: './',
@@ -13,7 +7,6 @@ const createJestConfig = nextJest({
 
 const config: Config = {
   coverageProvider: 'v8',
-  rootDir: __dirname, // caminho absoluto
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
