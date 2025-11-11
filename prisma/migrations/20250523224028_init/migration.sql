7825391fbcb82466b84f9abd7f72c7f166f51df2
Karina Peres <134704973+karinaperes@users.noreply.github.com>
2025-11-11 13:28:06 -0300

    Fix/correcoes status env (#391)
    * fix(status-concluido): correção status CONCLUIDO schema.prisma e tirei o acento de todos os arquivos
    
    * fix(arquivo-env): recriei o arquivo example.env

--- prisma/migrations/20250523224028_init/migration.sql
+++ prisma/migrations/20250523224028_init/migration.sql
@@ -7,7 +7,7 @@
 
 */
 -- CreateEnum
-CREATE TYPE "ProjectStatus" AS ENUM ('BUSCANDO', 'EM_ANDAMENTO', 'CONCLUÍDO');
+CREATE TYPE "ProjectStatus" AS ENUM ('BUSCANDO', 'EM_ANDAMENTO', 'CONCLUIDO');
 
 -- AlterTable
 ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
