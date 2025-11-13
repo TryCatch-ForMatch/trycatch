832fcb30df5dfc046acfac794b8c9b5ffa1bc617
Karina Peres <134704973+karinaperes@users.noreply.github.com>
2025-11-13 18:33:36 -0300

    feat(user-certificate-table): criação de nova tabela UserCertificate, o banco foi resetado (#404)
    criação de nova tabela UserCertificate, o banco foi resetado, arquivos de migration foram excluidos
    
    feat #396

--- prisma/migrations/20251102225813_update_user_availability_ismentor/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isMentor` on the `UserAvailability` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."UserAvailability" DROP COLUMN "isMentor";
