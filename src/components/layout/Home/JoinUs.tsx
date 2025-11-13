5564562c8675f068ffdb76cae70c0db361c1b5b2
Christian Furquim <102332171+Furquimn@users.noreply.github.com>
2025-11-13 17:04:58 -0300

    feat: cria a section JoinUs (#402)

---
+++ src/components/layout/Home/JoinUs.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import { MoveRight } from 'lucide-react';
+import Image from 'next/image';
+
+export default function JoinUs() {
+  return (
+    <section className="mt-20 mr-30 mb-50 ml-30">
+      <div className="flex h-64 w-full rounded-3xl bg-linear-to-r from-[#D5EEFF]/10 to-[#FFE6EC]/10">
+        <div className="mr-10 p-10">
+          <h2 className="mb-5 text-4xl">Participe da nossa rede agora</h2>
+          <p className="mb-5">
+            Cadastre seu projeto ou junte-se à comunidade e colabore em novas
+            ideias.
+          </p>
+          <Button className="rounded-full bg-[#35343C] py-5 pr-1 pl-6 text-lg shadow-md transition hover:bg-[#35343C]/90">
+            <span className="mr-3">Começar agora</span>
+          </Button>
+        </div>
+
+        <div>
+          <Image
+            src="/Icon_aboutUs.svg"
+            alt="ColabWork"
+            width={300}
+            height={300}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
