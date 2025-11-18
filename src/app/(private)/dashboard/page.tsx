62824cbda13534c7c4c585cd645e814644a0520c
Karina <karinaperesg@gmail.com>
2025-11-17 22:38:44 -0300

    refactor(admin-page-redirect): usuario role admin será direcionado para /dashboard/admin-page
    refactor #416

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,13 +1,26 @@
 import { Metadata } from 'next';
 import BasePage from '@/components/Dashboard/BasePage';
 import UnderDevelopment from '@/components/UnderDevelopment';
+import { checkAuth } from '@/lib/check-auth';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Dashboard - TryCatch',
   description: 'Bem vindo ao tryCatch',
 };
 
-export default function Page() {
+export default async function Page() {
+  const { authorized, session } = await checkAuth();
+
+  if (!authorized || !session) {
+    redirect('/login');
+  }
+
+  // admin → vai pra página ADM
+  if (session.user.role === 'ADMIN') {
+    redirect('/dashboard/admin-page');
+  }
+
   return (
     <BasePage>
       <h1>Dashboard</h1>
