2e2e78ec854d49acc1655f272cb7d0dd631ce48a
Karina Peres <134704973+karinaperes@users.noreply.github.com>
2025-12-25 20:35:00 -0300

    Refactor/home hero (#459)
    * refactor(hero): unifica layout e ajusta responsividade conforme figma
    
    * refactor(title): corrige semântica e organiza elementos decorativos
    
    * refactor(home-page): remove Hero2 após unificação do Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import AboutUs from '@/components/layout/Home/AboutUs';
 import Faq from '@/components/layout/Home/Faq';
-
 import Footer from '@/components/layout/Home/Footer';
 import Hero from '@/components/layout/Home/Hero';
-import Hero2 from '@/components/layout/Home/Hero2';
 import { HomeNavBar } from '@/components/layout/Home/HomeNavBar';
 import JoinUs from '@/components/layout/Home/JoinUs';
 
@@ -14,7 +12,6 @@ export default function HomePage() {
 
       <div className="px-5 py-5 md:px-7 lg:px-6 xl:px-10 xxl:px-[39px]">
         <Hero />
-        <Hero2 />
 
         <AboutUs />
 
