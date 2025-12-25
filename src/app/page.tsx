d63b3eea8bcbb7d4f03bed9473426fa2d517b555
Karina <karinaperesg@gmail.com>
2025-12-25 17:51:00 -0300

    fix(footer-rename): renomeei o arquivo footer que estava incorreto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import AboutUs from '@/components/layout/Home/AboutUs';
 import Faq from '@/components/layout/Home/Faq';
 
-import Footer from '@/components/layout/Home/Foooter';
+import Footer from '@/components/layout/Home/Footer';
 import Hero from '@/components/layout/Home/Hero';
 import Hero2 from '@/components/layout/Home/Hero2';
 import { HomeNavBar } from '@/components/layout/Home/HomeNavBar';
