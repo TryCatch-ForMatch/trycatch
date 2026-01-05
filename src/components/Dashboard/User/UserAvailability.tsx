fc1ada6e7d0fc03dbf145b9a298069789d280996
Christian Furquim <102332171+Furquimn@users.noreply.github.com>
2026-01-05 13:31:03 -0300

    refactor(useravailability.tsx): altera layout da aba configurações do dashboard (#480)
    re ##478

--- src/components/Dashboard/User/UserAvailability.tsx
+++ src/components/Dashboard/User/UserAvailability.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { useEffect } from 'react';
+import * as Switch from '@radix-ui/react-switch';
 import { useRouter } from 'next/navigation';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
+import { Moon } from 'lucide-react';
+
 import {
   Select,
   SelectTrigger,
@@ -38,8 +41,6 @@ const schema = z.object({
     .min(1, 'Selecione pelo menos um dia com horário'),
 });
 
-
-
 type FormData = {
   skills: string[];
   availabilities: IAvailability[];
@@ -47,7 +48,7 @@ type FormData = {
 
 export function UserAvailabilityForm() {
   const router = useRouter();
-  const {allSkills, allUserSkills } =useSkills()
+  const { allSkills, allUserSkills } = useSkills();
   const {
     control,
     register,
@@ -88,7 +89,6 @@ export function UserAvailabilityForm() {
     }
   };
 
-
   const onSubmit = async (data: FormData) => {
     try {
       const res = await fetch('/api/user-availability', {
@@ -113,10 +113,14 @@ export function UserAvailabilityForm() {
     }
   };
 
-  const availableSkills = allSkills.filter((s) => !selectedSkills.includes(s.id??"") && !allUserSkills.some(item=>item.skill?.id===s.id));
+  const availableSkills = allSkills.filter(
+    (s) =>
+      !selectedSkills.includes(s.id ?? '') &&
+      !allUserSkills.some((item) => item.skill?.id === s.id)
+  );
 
   return (
-    <Card className="mx-auto mt-6 max-w-4xl rounded-2xl p-6 shadow-lg">
+    <Card className="mx-auto mt-6 rounded-2xl p-6 shadow-lg">
       <CardContent>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           {/* Skills */}
@@ -135,7 +139,7 @@ export function UserAvailabilityForm() {
               </SelectTrigger>
               <SelectContent>
                 {availableSkills.map((skill) => (
-                  <SelectItem key={skill.id} value={skill?.id??""}>
+                  <SelectItem key={skill.id} value={skill?.id ?? ''}>
                     {skill.name}
                   </SelectItem>
                 ))}
@@ -149,7 +153,7 @@ export function UserAvailabilityForm() {
             <div className="mt-3 flex flex-wrap gap-2">
               {selectedSkills.map((skillId: string) => {
                 const skill = allSkills.find((s) => s.id === skillId);
-                
+
                 return (
                   <span
                     key={skillId}
@@ -174,43 +178,58 @@ export function UserAvailabilityForm() {
             </div>
           </div>
 
-            {/* Mostrando minha Skills */}
-            <Label className='font-medium'>Minhas Skils {allUserSkills.length ||0}</Label>
-            <div className='flex items-start gap-3 font-normal'>
-            {
-              allUserSkills.map((item)=>(
-                <div key={item.id} className='flex items-center gap-1 rounded-full bg-gray-200 px-3 py-1 text-base'>
-                  <span className=''>{item.skill?.name}</span>
-                </div>
-              ))
-            }
+          {/* Mostrando minha Skills */}
+          <Label className="font-medium">
+            Minhas Skils {allUserSkills.length || 0}
+          </Label>
+          <div className="flex items-start gap-3 font-normal">
+            {allUserSkills.map((item) => (
+              <div
+                key={item.id}
+                className="flex items-center gap-1 rounded-full bg-gray-200 px-3 py-1 text-base"
+              >
+                <span className="">{item.skill?.name}</span>
+              </div>
+            ))}
           </div>
           {/* Dias da semana e horários */}
           <div>
-            <Label className="mb-2 block font-semibold text-gray-700">
-              Dias da semana e horários
+            <Label className="mt-6 mb-2 block font-semibold text-gray-700">
+              Disponibilidade de horários
+              <p className="mt-2 text-sm">
+                Configure os períodos em que você poderá trabalhar em projetos,
+                mantendo sua agenda clara e organizada.
+              </p>
             </Label>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <div className="mt-6 flex flex-col gap-2">
               {weekdays.map((day, i) => {
                 const field = fields.find((f) => f.weekday === i);
                 return (
-                  <div key={i} className="flex flex-col rounded-lg border p-3">
-                    <label className="flex cursor-pointer items-center space-x-4">
-                      <input
-                        type="checkbox"
+                  <div
+                    key={i}
+                    className="grid grid-cols-[160px_1fr] items-center gap-6 rounded-lg p-3"
+                  >
+                    <Label className="flex cursor-pointer items-center gap-4">
+                      <Switch.Root
                         checked={!!field}
-                        onChange={() => onToggleWeekday(i)}
-                        className="h-5 w-5 cursor-pointer rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                      <span className="ml-2 text-base font-medium">{day}</span>
-                    </label>
+                        onCheckedChange={() => onToggleWeekday(i)}
+                        className="relative h-6 w-11 rounded-full bg-gray-300 transition data-[state=checked]:bg-indigo-600"
+                      >
+                        <Switch.Thumb className="block h-4 w-4 translate-x-1 rounded-full bg-white transition data-[state=checked]:translate-x-6" />
+                      </Switch.Root>
 
-                    {field && (
-                      <div className="mt-3 flex space-x-3">
-                        <div className="flex flex-col">
-                          <Label className="mb-1 text-sm">Início</Label>
-                          <Input
+                      <span className="text-base font-medium">{day}</span>
+                    </Label>
+
+                    {field ? (
+                      <div className="mt-3 flex items-center gap-14">
+                        <div className="flex h-10 items-center gap-36 rounded-xl border px-2.5">
+                          <label className="text-sm text-gray-400/70">
+                            Das
+                          </label>
+                          <input
                             type="time"
+                            className="border-none shadow-none"
                             {...register(
                               `availabilities.${fields.indexOf(field)}.startTime` as const,
                               { required: true }
@@ -219,10 +238,11 @@ export function UserAvailabilityForm() {
                           />
                         </div>
 
-                        <div className="flex flex-col">
-                          <Label className="mb-1 text-sm">Término</Label>
-                          <Input
+                        <div className="flex h-10 items-center gap-48 rounded-xl border px-2.5">
+                          <label className="text-sm text-gray-400/70">Às</label>
+                          <input
                             type="time"
+                            className="border-none shadow-none"
                             {...register(
                               `availabilities.${fields.indexOf(field)}.endTime` as const,
                               { required: true }
@@ -231,6 +251,10 @@ export function UserAvailabilityForm() {
                           />
                         </div>
                       </div>
+                    ) : (
+                      <span className="ml-20 flex gap-4 text-sm text-gray-400/70">
+                        <Moon /> Indisponível
+                      </span>
                     )}
                   </div>
                 );
@@ -241,6 +265,12 @@ export function UserAvailabilityForm() {
                 {errors.availabilities.message}
               </p>
             )}{' '}
+            <p className="mt-6 text-sm">
+              Os horários que você configurar serão disponibilizados no seu
+              perfil da plataforma. Isso ajuda outros colaboradores a visualizar
+              seus períodos livres, otimizar convites para projetos e planejar
+              interações de forma mais eficiente.
+            </p>
           </div>
 
           {/* Submit */}
